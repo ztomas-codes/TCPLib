4e93affe5266d0d3905782d95488abb256464062
Tomaš Zbedina <tomaszbedina@gmail.com>
2023-10-14 14:20:16 +0200

    Base of library

--- TCPLib/main.cpp
+++ TCPLib/main.cpp
@@ -6,6 +6,33 @@
 int main()
 {
 	std::cout << "Hello World!" << std::endl;
-	TCPClient client("172.0.0.1");
+	TCPListener server = TCPListener();
+	if (server.StartListening() != SOCKET_ERROR)
+	{
+		std::cout << "Server started" << std::endl;
+		SOCKET client = server.acceptClient();
+		if (client == INVALID_SOCKET)
+		{
+			std::cout << "Failed to accept client" << WSAGetLastError() << std::endl;
+		}
+		else
+		{
+			std::cout << "Client accepted" << std::endl;
+			char buffer[1024];
+			int result = recv(client, buffer, sizeof(buffer), 0);
+			if (result == SOCKET_ERROR)
+			{
+				std::cout << "Failed to receive data" << WSAGetLastError() << std::endl;
+			}
+			else
+			{
+				std::cout << "Received data: " << buffer << std::endl;
+			}
+		}
+	}
+	else
+	{
+		std::cout << "Server failed to start" << WSAGetLastError() << std::endl;
+	}
 	return 0;
 }
--- TCPLib/tcplib.h
+++ TCPLib/tcplib.h
@@ -1,16 +1,142 @@
 #pragma once
-#include<stdio.h>
-#include<iostream>
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <string.h>
+
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+#define DEFAULT_BUFLEN 512
+const int PROTOCOL_TCP = 0;
+const int PROTOCOL_UDP = 1;
+
+class SocketFactory
+{
+	protected:
+		std::string Ip;
+		UINT16 Port;
+
+		SOCKET _socket;
+		WSADATA _wsaData;
+		sockaddr_in _socketAdrr;
+
+		int LoadDLL()
+		{
+			int result = WSAStartup(MAKEWORD(2, 2), &_wsaData);
+			return result;
+		}
+		int CreateSocket(int protocol)
+		{
+			if (protocol == PROTOCOL_TCP) _socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+			else if (protocol == PROTOCOL_UDP) _socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+			else std::cout << "Invalid protocol" << std::endl;
+			return _socket;
+		}
 
 
-class TCPClient
+		void CreateSocketAddress()
+		{
+			_socketAdrr.sin_family = AF_INET;
+			
+			PCWSTR ip = (PCWSTR)Ip.c_str();
+
+			InetPton(AF_INET, ip, &_socketAdrr.sin_addr.s_addr);
+			_socketAdrr.sin_port = htons(Port);
+
+		}
+};
+
+class TCPClient : private SocketFactory
+{
+	private:
+		int Protocol;
+	public:
+		TCPClient(std::string Ip, UINT16 port, int protocol = PROTOCOL_TCP)
+		{
+			Ip = Ip;
+			Port = port;
+			Protocol = protocol;
+		}
+		int Connect()
+		{
+			if (LoadDLL() != 0) {
+				std::cout << "Failed to load DLL" << std::endl;
+				return 1;
+			}
+
+			if (CreateSocket(Protocol) == INVALID_SOCKET)
+			{
+				std::cout << "Failed to create socket" << std::endl;
+				WSACleanup();
+				return 1;
+			}
+
+			CreateSocketAddress();
+
+			return connect(_socket, (SOCKADDR*)&_socketAdrr, sizeof(_socketAdrr));
+		}
+	
+	
+};
+
+
+class TCPListener : private SocketFactory
 {
 private:
-	std::string m_ip;
-public:
-	TCPClient(std::string ip)
+	UINT16 Port;
+	int Protocol;
+	std::string Ip;
+
+	int BindSocket()
 	{
-		m_ip = ip;
-		std::cout << "TCPClient created with ip: " << m_ip << std::endl;
+
+
+		return bind(_socket, (SOCKADDR*)&_socketAdrr, sizeof(_socketAdrr));
+	}
+
+public:
+	TCPListener(UINT16 port, int protocol = PROTOCOL_TCP)
+	{
+		Ip = "172.0.0.1";
+		Port = port;
+		Protocol = protocol;
+	}
+	int StartListening()
+	{
+		if (LoadDLL() != 0) {
+			std::cout << "Failed to load DLL" << std::endl;
+			return 1;
+		}
+
+		if (CreateSocket(Protocol) == INVALID_SOCKET)
+		{
+			std::cout << "Failed to create socket" << std::endl;
+			WSACleanup();
+			return 1;
+		}
+
+		CreateSocketAddress();
+
+		if (BindSocket() == SOCKET_ERROR)
+		{
+			std::cout << "Failed to bind socket: " << WSAGetLastError() << std::endl;
+			closesocket(_socket);
+			WSACleanup();
+			return 1;
+		}
+
+		return listen(_socket, 2);
+	}
+	SOCKET acceptClient()
+	{
+		return accept(_socket, NULL, NULL);
 	}
 };
